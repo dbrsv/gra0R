e39aa47b7e0c0cd823aa95a3fd9b80aacd5a107c
dbrsv <despotum@mail.ru>
2022-04-29 20:32:32 +0300

    Update SALData.R

--- SALData.R
+++ SALData.R
@@ -13,25 +13,25 @@
 ## 
 
 
-# Подключение библиотек || LIBRARIES-----------------------------
+# РџРѕРґРєР»СЋС‡РµРЅРёРµ Р±РёР±Р»РёРѕС‚РµРє || LIBRARIES-----------------------------
 library(readr)
 library(stringi)
 library(magrittr)
 library(SciViews)
 
-# Выбор папки с файлами *.dat || CHOOSE A FOLDER WITH *.DAT FILES-----
+# Р’С‹Р±РѕСЂ РїР°РїРєРё СЃ С„Р°Р№Р»Р°РјРё *.dat || CHOOSE A FOLDER WITH *.DAT FILES-----
 if (interactive() && .Platform$OS.type == "windows")
   path <- choose.dir()
 
-#ПОДГОТОВКА ДАННЫХ-----------------------------------------------
-# Вектор с полными именами всех dat-файлов в указанной папке || FULL NAMES OF DAT-FILES IN THE FOLDER
+#РџРћР”Р“РћРўРћР’РљРђ Р”РђРќРќР«РҐ-----------------------------------------------
+# Р’РµРєС‚РѕСЂ СЃ РїРѕР»РЅС‹РјРё РёРјРµРЅР°РјРё РІСЃРµС… dat-С„Р°Р№Р»РѕРІ РІ СѓРєР°Р·Р°РЅРЅРѕР№ РїР°РїРєРµ || FULL NAMES OF DAT-FILES IN THE FOLDER
 
 fn_full <- dir(path, pattern = "*.dat", full.names = T)
 
-# Вектор с краткими именами всех dat-файлов в указанной папке || SHORT NAMES OF DAT-FILES IN THE FOLDER
+# Р’РµРєС‚РѕСЂ СЃ РєСЂР°С‚РєРёРјРё РёРјРµРЅР°РјРё РІСЃРµС… dat-С„Р°Р№Р»РѕРІ РІ СѓРєР°Р·Р°РЅРЅРѕР№ РїР°РїРєРµ || SHORT NAMES OF DAT-FILES IN THE FOLDER
 fn <- basename(fn_full)
 
-# Импорт данных из файлов в единый список || DATA IMPORT FROM FILES TO A LIST
+# РРјРїРѕСЂС‚ РґР°РЅРЅС‹С… РёР· С„Р°Р№Р»РѕРІ РІ РµРґРёРЅС‹Р№ СЃРїРёСЃРѕРє || DATA IMPORT FROM FILES TO A LIST
 dat <- lapply(fn_full, 
               readr::read_delim, 
               delim = ":", 
@@ -39,30 +39,30 @@ dat <- lapply(fn_full,
               col_names = F, 
               trim_ws = T)
 
-# вектор с границами гранулометрических классов || BOUNDARIES BETWEEN GRAIN-SIZE CLASSES 
+# РІРµРєС‚РѕСЂ СЃ РіСЂР°РЅРёС†Р°РјРё РіСЂР°РЅСѓР»РѕРјРµС‚СЂРёС‡РµСЃРєРёС… РєР»Р°СЃСЃРѕРІ || BOUNDARIES BETWEEN GRAIN-SIZE CLASSES 
 gs <- as.vector(t(dat[[1]][,1]))
 
-# Подсчет количества повторных анализов для каждого образца 
+# РџРѕРґСЃС‡РµС‚ РєРѕР»РёС‡РµСЃС‚РІР° РїРѕРІС‚РѕСЂРЅС‹С… Р°РЅР°Р»РёР·РѕРІ РґР»СЏ РєР°Р¶РґРѕРіРѕ РѕР±СЂР°Р·С†Р° 
 # || ESTIMATION OF ANALYSIS ITERATIONS FOR EACH SAMPLE
 
-  # паттерны и значения для замены в кратких именах фалов для приведения их к единому виду ||
+  # РїР°С‚С‚РµСЂРЅС‹ Рё Р·РЅР°С‡РµРЅРёСЏ РґР»СЏ Р·Р°РјРµРЅС‹ РІ РєСЂР°С‚РєРёС… РёРјРµРЅР°С… С„Р°Р»РѕРІ РґР»СЏ РїСЂРёРІРµРґРµРЅРёСЏ РёС… Рє РµРґРёРЅРѕРјСѓ РІРёРґСѓ ||
   # PATTERNS & REPLACEMENTS FOR THE FORMATING OF 
 patterns <- c(paste("_", c(1:6), ".dat", sep = ""),
-              "АНС-", "АНС", "_bulk", "_rem")
+              "РђРќРЎ-", "РђРќРЎ", "_bulk", "_rem")
 replacements <- c(rep("", 6), "ANS", "ANS", "", "")
-  # вектор с форматированными краткими именами файлов (ID образов) ||
+  # РІРµРєС‚РѕСЂ СЃ С„РѕСЂРјР°С‚РёСЂРѕРІР°РЅРЅС‹РјРё РєСЂР°С‚РєРёРјРё РёРјРµРЅР°РјРё С„Р°Р№Р»РѕРІ (ID РѕР±СЂР°Р·РѕРІ) ||
   # VECTOR WITH FORMATTED SHORT NAMES OF THE DAT-FILES (SAMPLE IDs)
 s_id <- stri_replace_all_regex(fn,
                                pattern = patterns,
                                replacement = replacements,
                                vectorize=F)
-  # вектор со значениями повторений аналиов для каждого образца в порядке, соотв. вектору fn
+  # РІРµРєС‚РѕСЂ СЃРѕ Р·РЅР°С‡РµРЅРёСЏРјРё РїРѕРІС‚РѕСЂРµРЅРёР№ Р°РЅР°Р»РёРѕРІ РґР»СЏ РєР°Р¶РґРѕРіРѕ РѕР±СЂР°Р·С†Р° РІ РїРѕСЂСЏРґРєРµ, СЃРѕРѕС‚РІ. РІРµРєС‚РѕСЂСѓ fn
   # VECTOR WITH MEASUREMENT ITERATIONS FOR EACH SAMPLE IN ORDER SIMILAR TO fn VECTOR
 s <- table(s_id)[match(unique(s_id), names(table(s_id)))]
-  # имена значений вектора <- ID образцов || NAMES OF VECTOR VALUES <- SAMPLE IDs
+  # РёРјРµРЅР° Р·РЅР°С‡РµРЅРёР№ РІРµРєС‚РѕСЂР° <- ID РѕР±СЂР°Р·С†РѕРІ || NAMES OF VECTOR VALUES <- SAMPLE IDs
 names(s) <- unique(s_id)
 
-# дата фрейм с данными НЕ кумулятивных распределений || DATAFRAME WITH ALL THE NON-CUMULATIVE DISTRIBUTIONS
+# РґР°С‚Р° С„СЂРµР№Рј СЃ РґР°РЅРЅС‹РјРё РќР• РєСѓРјСѓР»СЏС‚РёРІРЅС‹С… СЂР°СЃРїСЂРµРґРµР»РµРЅРёР№ || DATAFRAME WITH ALL THE NON-CUMULATIVE DISTRIBUTIONS
 gs_dist_all <- data.frame(gs)
 
 for(i in 1:length(dat)){
@@ -70,22 +70,22 @@ for(i in 1:length(dat)){
 }
 colnames(gs_dist_all) <- c('size um', fn)
 
-# дата фрейм с данными кумулятивных распределений || DATAFRAME WITH ALL THE CUMULATIVE DISTRIBUTIONS
+# РґР°С‚Р° С„СЂРµР№Рј СЃ РґР°РЅРЅС‹РјРё РєСѓРјСѓР»СЏС‚РёРІРЅС‹С… СЂР°СЃРїСЂРµРґРµР»РµРЅРёР№ || DATAFRAME WITH ALL THE CUMULATIVE DISTRIBUTIONS
 gs_dist_cumm_all <- data.frame(gs)
 for(i in 1:length(dat)){
   gs_dist_cumm_all <- cbind(gs_dist_cumm_all, dat[[i]][, 2])
 }
 colnames(gs_dist_cumm_all) <- c('size um', fn)
 
-#ОСРЕДНЕНИЕ ДАННЫХ || DATA AVERAGING--------------------------------------
-  # номер начального остолбца с гран.данными для выборок из датафрейма ||
+#РћРЎР Р•Р”РќР•РќРР• Р”РђРќРќР«РҐ || DATA AVERAGING--------------------------------------
+  # РЅРѕРјРµСЂ РЅР°С‡Р°Р»СЊРЅРѕРіРѕ РѕСЃС‚РѕР»Р±С†Р° СЃ РіСЂР°РЅ.РґР°РЅРЅС‹РјРё РґР»СЏ РІС‹Р±РѕСЂРѕРє РёР· РґР°С‚Р°С„СЂРµР№РјР° ||
   # THE NUMBER OF THE FIRST COLUMN WITH GS-DATA
 start_col <- 2
-  #номера начальных столбцов для остальных выборок из датафрейма ||
+  #РЅРѕРјРµСЂР° РЅР°С‡Р°Р»СЊРЅС‹С… СЃС‚РѕР»Р±С†РѕРІ РґР»СЏ РѕСЃС‚Р°Р»СЊРЅС‹С… РІС‹Р±РѕСЂРѕРє РёР· РґР°С‚Р°С„СЂРµР№РјР° ||
   # COLUMN NUMBERS RELATED TO THE BEGINNING OF EACH MEASUREMENT SERIES
 subset_from <- as.integer(c(start_col, cumsum(s)+start_col))
 
-#датафрейм для осредненных не кумулятивных распределений
+#РґР°С‚Р°С„СЂРµР№Рј РґР»СЏ РѕСЃСЂРµРґРЅРµРЅРЅС‹С… РЅРµ РєСѓРјСѓР»СЏС‚РёРІРЅС‹С… СЂР°СЃРїСЂРµРґРµР»РµРЅРёР№
 gs_mean_dist <- data.frame(gs)
 for(i in 1:length(s)){
   from <- subset_from[i]
@@ -111,7 +111,7 @@ for(i in 1:length(s)){
 }
 colnames(gs_mean_dist_cumm) <- c('size um', names(s))
 
-# ЗАПИСЬ ОСРЕДНЕННЫХ ДАННЫХ В ФАЙЛЫ || WRITTING THE AVERAGED DATA TO FILES--------
+# Р—РђРџРРЎР¬ РћРЎР Р•Р”РќР•РќРќР«РҐ Р”РђРќРќР«РҐ Р’ Р¤РђР™Р›Р« || WRITTING THE AVERAGED DATA TO FILES--------
 
 write.table(gs_mean_dist, 
             file = paste(path,"//ANS45-35_av_bulk.dat", 
